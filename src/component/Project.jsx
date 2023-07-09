9701de116608a657047729729b199358c38cf88c
mirnaiem <mirnaiem8teen@gmail.com>
2023-07-09 23:48:24 +0600

    responsive more

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -8,14 +8,14 @@ const Project = () => {
   <div className="my-20">
    <h2 className="text-white text-5xl text-center font-bold mb-6">My Projects</h2>
 
-   <div className="grid grid-cols-3 gap-2 w-full">
-    <div className="bg-[#191919]  p-6  h-[500px] rounded-3xl ">
+   <div className="grid md:grid-cols-3 mx-aut0 gap-2 ">
+    <div className="bg-[#191919] p-10  pb-16  md:p-6  h-[500px] w-[85%] md:w-full rounded-3xl ">
      <div className="rounded-2xl w-full bg-[url('https://i.ibb.co/JQnHRTW/assignment-12-72b76-web-app-1.png')] bg-cover h-[70%]  ease-in-out duration-[5s]  hover:bg-bottom  " ></div>
     
      <h2 className="text-[#8c8c8c] text-xl mt-6">MERN Stack Project</h2>
      <h1 className="text-white text-2xl font-bold">Language School</h1>
 
-     <div className="flex justify-between gap-6 items-center ">
+     <div className="flex flex-col md:flex-row justify-between md:gap-6 items-center ">
       <div className="flex mt-3 gap-2">
        <Link className="flex items-center gap-2 h-8 bg-[#252525] px-2 rounded-lg m-0 text-white" to="https://github.com/mirnaiem/language-lab-client">Client<FaGithub></FaGithub></Link>
        <Link className="flex items-center gap-2 h-8 bg-[#252525] px-2 rounded-lg m-0 text-white" to="https://github.com/mirnaiem/language-lab-server">Server<FaGithub></FaGithub></Link>
@@ -25,13 +25,13 @@ const Project = () => {
       <Arrow></Arrow>
      </div>
     </div>
-    <div className="bg-[#191919]  p-6  h-[500px] rounded-3xl ">
+    <div className="bg-[#191919] p-2 pb-16  md:p-6  h-[500px] w-[85%] md:w-full  rounded-3xl ">
      <div className="rounded-2xl w-full bg-[url('https://i.ibb.co/RNbYCd5/assignment-11-auth-a17e0-web-app.png')] bg-cover h-[70%]  ease-in-out duration-[5s]  hover:bg-bottom  " ></div>
     
      <h2 className="text-[#8c8c8c] text-xl mt-6">FullStack Project</h2>
      <h1 className="text-white text-2xl font-bold">Toy Shop</h1>
 
-     <div className="flex justify-between gap-6 items-center ">
+     <div className="flex flex-col md:flex-row justify-between md:gap-6 items-center ">
       <div className="flex mt-3 gap-2">
        <Link className="flex items-center gap-2 h-8 bg-[#252525] px-2 rounded-lg m-0 text-white" to="https://github.com/mirnaiem/toy-shop-client">Client<FaGithub></FaGithub></Link>
        <Link className="flex items-center gap-2 h-8 bg-[#252525] px-2 rounded-lg m-0 text-white" to="https://github.com/mirnaiem/toy-shop-server">Server<FaGithub></FaGithub></Link>
@@ -41,13 +41,13 @@ const Project = () => {
       <Arrow></Arrow>
      </div>
     </div>
-    <div className="bg-[#191919]  p-6 h-[500px] rounded-3xl ">
+    <div className="bg-[#191919] p-2 pb-16  md:p-6 h-[500px] w-[85%] md:w-full  rounded-3xl ">
      <div className="rounded-2xl w-full bg-[url('https://i.ibb.co/DGYLPYH/assignment-10-auth-c0d24-web-app.png')] bg-cover h-[70%]  ease-in-out duration-[5s]  hover:bg-bottom  " ></div>
     
      <h2 className="text-[#8c8c8c] text-xl mt-6">FullStack Project</h2>
      <h1 className="text-white text-2xl font-bold">Awesome Chef</h1>
 
-     <div className="flex justify-between gap-6 items-center ">
+     <div className="flex flex-col md:flex-row justify-between md:gap-6 items-center ">
       <div className="flex mt-3 gap-2">
        <Link className="flex items-center gap-2 h-8 bg-[#252525] px-2 rounded-lg m-0 text-white" to="https://github.com/mirnaiem/awesome-chef-client">Client<FaGithub></FaGithub></Link>
        <Link className="flex items-center gap-2 h-8 bg-[#252525] px-2 rounded-lg m-0 text-white" to="https://github.com/mirnaiem/awesome-chef-server">Server<FaGithub></FaGithub></Link>
